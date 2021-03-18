9dcd5a3a5145bd544e365e412a1661775bcc260d
joeniofarias <joenio.farias@gmail.com>
2021-03-18 19:53:16 +0000

    atualizacao1

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
 
-@app.route("/")
-@app.route("/index")
+
+@app.route('/')
+@app.route('/index')
 def index():
-    return "Olá Mundo"
+    user = {'username': 'Feulo'}
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
